5107681cb1c84827ab7d90773bf4bc616525de22
HamaadYousaf <hamaaadyousaf@outlook.com>
2024-08-11 14:38:53 -0400

    database

--- cmd/task/main.go
+++ cmd/task/main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
-	tasklist "github.com/HamaadYousaf/Task-List"
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	godotenv.Load()
 
-	projectList := &tasklist.ProjectList{}
+	connStr := os.Getenv("DATABASE_URL")
 
-	projectList.AddProject("test")
-	projectList.AddProject("test2")
-	projectList.AddProject("test3")
-	projectList.AddTask("test task", 0)
-	projectList.AddTask("test task2", 0)
-	projectList.AddTask("test task3", 1)
+	db, err := sql.Open("postgres", connStr)
 
-	projectList.SetPriority("high", 0, 1)
-	projectList.SetStatus("done", 0, 0)
-	// projectList.DeleteTask(0, 0)
-	// projectList.DeleteTask(0, 0)
-	// projectList.DeleteProject(2)
-	projectList.ListProjects()
-	projectList.ListTasks(0)
-	// projectList.ListTasks(1)
-	// projectList.ListTasks(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	// projectList := &tasklist.ProjectList{}
+
+	// projectList.AddProject("test")
+	// projectList.AddProject("test2")
+	// projectList.AddProject("test3")
+	// projectList.AddTask("test task", 0)
+	// projectList.AddTask("test task2", 0)
+	// projectList.AddTask("test task3", 1)
+
+	// projectList.SetPriority("high", 0, 1)
+	// projectList.SetStatus("done", 0, 0)
+	// // projectList.DeleteTask(0, 0)
+	// // projectList.DeleteTask(0, 0)
+	// // projectList.DeleteProject(2)
+	// projectList.ListProjects()
+	// projectList.ListTasks(0)
+	// // projectList.ListTasks(1)
+	// // projectList.ListTasks(2)
 }
